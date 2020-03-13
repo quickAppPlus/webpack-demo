49a687268f7f0adb900ae6a1e1345d4f9c33e0e3
pengjie <37610029@qq.com>
2020-03-13 23:21:13 +0800

    回退处理为管理输出准备

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.jpg";
-import Data from "./data.xml";
 
 function component() {
   const element = document.createElement("div");
-
-  // lodash，现在由此脚本导入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  // 使用了两次 icon ，一次作为节点插入，一次作为元素背景（style.css里面）
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
