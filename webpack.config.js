84e315c6cc3d5fa77fb126bbe630ccfc94963915
pengjie <37610029@qq.com>
2020-03-15 12:03:42 +0800

    06 development 使用 source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js"
   },
+  devtool: "inline-source-map",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "管理输出"
+      title: "Development"
     })
   ],
   output: {
