5945f54588f02dd6d42ae81d6b02975d23e10a40
pengjie <37610029@qq.com>
2020-03-15 17:00:44 +0800

    06 development 问题

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ function component() {
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
+  btn.onclick = printMe; // onclick 事件绑定原始的 printMe 函数上
 
   element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept("./print.js", function() {
     console.log("Accepting the updated printMe module!");
-    printMe();
+    document.body.removeChild(element);
+    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
+    document.body.appendChild(element);
   });
 }
