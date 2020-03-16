519b73925b88643ebca9a6649807a3fa732ae65f
pengjie <37610029@qq.com>
2020-03-16 22:06:15 +0800

    08 tree shaking 将文件标记为 side-effect-free(无副作用)

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/index.js"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  plugins: [
-    // 对于 CleanWebpackPlugin 的 v2 versions 以下版本，使用 new CleanWebpackPlugin(['dist/*'])
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "模块热替换"
-    })
-  ],
+  entry: "./src/index.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  optimization: {
+    usedExports: true
   }
 };
