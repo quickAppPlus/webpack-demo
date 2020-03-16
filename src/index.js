d382b7bbc19477a1487cf80547492266a9cfd907
pengjie <37610029@qq.com>
2020-03-16 22:55:06 +0800

    10 code-splitting prevent duplication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import { cube } from "./math.js";
+import _ from "lodash";
+
+console.log(_.join(["a", "b", "c!"], " "));
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 function component() {
   const element = document.createElement("pre");
-
-  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
-    "\n\n"
-  );
 
   return element;
 }
