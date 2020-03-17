2184d08f4fe95e93fbfeed4810bf53c63f1f6741
pengjie <37610029@qq.com>
2020-03-17 22:34:15 +0800

    12 caching output filename

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js"
-  },
+  entry: "./src/index.js",
+  plugins: [
+    // 对于 CleanWebpackPlugin 的 v2 versions 以下版本，使用 new CleanWebpackPlugin(['dist/*'])
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Caching"
+    })
+  ],
   output: {
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist")
   }
 };
